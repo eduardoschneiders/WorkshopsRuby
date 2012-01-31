2364a4d017e137bfb3204a3826643faf6b1f7563
Eduardo Matheus Schneiders <eduardo.m.schneiders@gmail.com>
2012-01-30 22:43:26 -0200

    terminei

---
+++ exercises/conta.rb
@@ -0,0 +1,21 @@
+puts "1 - " 
+puts "Hello"
+
+puts "2 - " 
+puts "teste".delete("e")
+
+puts "3 - " 
+puts "i'm the walruls".include?("teste")
+
+puts "4 - "
+puts "i'm the walruls".include?("teste")
+
+puts "5 - " 
+puts "i'm the walruls at wall".include?("wall")
+puts " "
+puts " "
+puts " "
+word = 'stressed'
+reversedWord = word.reverse
+word2 = 'desserts'
+puts reversedWord == word2
