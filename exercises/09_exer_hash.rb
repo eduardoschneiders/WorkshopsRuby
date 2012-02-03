48fe04bea471927f572794007eee8431d0b9f34e
Eduardo Matheus Schneiders <eduardo.m.schneiders@gmail.com>
2012-02-03 16:57:59 -0200

    Exercícios com hash

---
+++ exercises/09_exer_hash.rb
@@ -0,0 +1,11 @@
+carros = Array.new()
+carros = [
+
+		{"marca" => 'valor', "ano" => 'valor'},
+		{"marca" => 'valor', "ano" => 'valor'},
+		{"marca" => 'valor', "ano" => 'valor'}
+
+]
+
+puts carros
+
