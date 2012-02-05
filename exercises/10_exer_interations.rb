a0ad853fdb44bebce3b57a913627586207485b2a
Eduardo Matheus Schneiders <eduardo.m.schneiders@gmail.com>
2012-02-04 22:43:03 -0200

    Exercício com interações

--- exercises/10_exer_interations.rb
+++ exercises/10_exer_interations.rb
@@ -1,2 +1,18 @@
-jogo do acerta o nuumero sorteado
-	
+# encoding: UTF-8
+
+range = (0..100)
+numGuess = rand(range.first..range.last)
+
+puts "Digite um numero de #{range.first} até #{range.last}: "
+numUser = gets.to_i
+
+while true
+	if numUser != numGuess
+		tip = numGuess > numUser ? "maior" : "menor"
+		puts "Número errado, digite outro #{tip}: "
+		numUser = gets.to_i
+	else
+		puts "Você acertou com o numero #{numGuess}"
+		break
+	end
+end
