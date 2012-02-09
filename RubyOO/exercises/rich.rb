c96b6265d153e00dfc7ec3f2df6fac66147dfb59
Eduardo Matheus Schneiders <eduardo.m.schneiders@gmail.com>
2012-02-09 14:11:26 -0200

    Calculating fortune by year, richer_than by year, Diference by year

--- RubyOO/exercises/rich.rb
+++ RubyOO/exercises/rich.rb
@@ -7,27 +7,27 @@ class Rich
 		@name, @fortune, @age, @born, @companies = args
 	end
 
-	def richer_than? other
+	def richer_than? other, year
 
 		if other.class == self.class
-			self.fortune > other.fortune
+			self.fortune[year] > other.fortune[year]
 		else
 			false
 		end
 
 	end
-
-	def fortune year
-		@fortune[year]
+	
+	def difference first_year, second_year
+		(self.fortune[second_year]) - (self.fortune[first_year])
 	end
 
 end
 
 
-gates = Rich.new "Gates", {2011 => 56}, 21, "Taquara", %w{TCA Azaléia}
-mittal = Rich.new "Mittal",{2011 => 31.1}, 4_000, 50, "Santa Cruz do Sul", %w{Azaléia Afubra}
-eike = Rich.new "eike", {2011 => 30}, 50, "Santa Cruz do Sul", %w{Azaléia Afubra}
-slim = Rich.new "slim", {2011 => 74}, 50, "Santa Cruz do Sul", %w{Azaléia Afubra}
+gates = Rich.new "Gates", {2011 => 56, 2010 => 53, 2009 => 40}, 21, "Taquara", %w{TCA Azaléia}
+mittal = Rich.new "Mittal",{2011 => 31.1, 2010 => 28.7, 2009 => 19.3}, 4_000, 50, "Santa Cruz do Sul", %w{Azaléia Afubra}
+eike = Rich.new "eike", {2011 => 30, 2010 => 27}, 50, "Santa Cruz do Sul", %w{Azaléia Afubra}
+slim = Rich.new "slim", {2011 => 74, 2010 => 53.5, 2009 => 35}, 50, "Santa Cruz do Sul", %w{Azaléia Afubra}
 
-puts gates.fortune 2011
-#puts eduardo.richer_than?  leoni
+puts gates.difference 2009, 2011 
+puts gates.richer_than? slim, 2010
